b0cb4e2b619f01daead00bb313e130235f02f123
imran-nazir-0011 <imran20001110@gmail.com>
2025-02-23 20:05:56 +0530

    final version

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def predict_datapoints():
             return render_template('predict.html', result=f"Error occurred : {e}", unique_values=unique_values)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
